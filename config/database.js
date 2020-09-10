67454237ac6164522c5df185e28e136a798c9dd5
DESKTOP-M2NEIG7\oco <mooo4030@naver.com>
2020-09-10 09:04:33 +0900

    database세팅완료

--- config/database.js
+++ config/database.js
@@ -1,20 +1,7 @@
 const mysql = require('mysql2/promise')
+const {info} = require('../mysql/mysql')
 
-const pool = mysql.createPool({
-    host: 'mm4030.cafe24.com',
-    port: '3306',
-    user: 'mm4030',
-    password: 'seoha123!',
-    database: 'mm4030',
-    connectionLimit: 10,
-    multipleStatements: true,
-    typeCast: function (field, next) {
-        if (field.type == 'VAR_STRING') {
-            return field.string();
-        }
-        return next();
-    }
-})
+const pool = mysql.createPool(info)
 
 const data = pool.query('set names utf8;')
 
