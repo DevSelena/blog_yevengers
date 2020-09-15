3373b560d520a0a3f30597fccae8dcc59cef4e5f
sonyejun <ywson9027@otheon.net>
2020-09-15 22:16:04 +0900

    index 1차

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
+    entry: {
+        style: "./public/css/style.less",
+    },
+    output: {
+        filename: '[name].js?[chunkhash]',
+        path: path.resolve(__dirname + '/dist'), 
+        publicPath: "../"
+    },
     module: {
       rules: [
         {
