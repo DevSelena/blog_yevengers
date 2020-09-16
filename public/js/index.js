5160b6eae6608386fc7ff8364a962eb68bd4be56
sonyejun <ywson9027@otheon.net>
2020-09-16 23:41:19 +0900

    IndexSH

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,16 @@
-function MakersClick(maker,name){
+//user선택
+function MakersClick(maker,name,duties){
     maker.setAttribute('class',name + ' ' + 'clickOff');
-    const makers = document.getElementsByClassName("clickOn");
-    for(let i in makers){
+
+    const makers = document.getElementsByClassName("Maker_clickOn");
+    for(let i = 0; i<makers.length; i++){
         makers[i].style.display = "none"; 
     }
+
+    let title = document.getElementById('title');
+    title.classList.add('clickOn');
+    title.innerText = duties;
+
+    document.getElementById('memberList').classList.add('on');
+    document.getElementById(name+'Info').style.display = "block";
 }
