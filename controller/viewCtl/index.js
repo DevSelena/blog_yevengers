fbfbebd9ec7ce6ec7c471216a47ee42790313146
sonyejun <62229757+sonyejun@users.noreply.github.com>
2020-09-17 15:32:05 +0900

    index complete

--- controller/viewCtl/index.js
+++ controller/viewCtl/index.js
@@ -7,4 +7,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'YEVENGERS' });
 });
 
+router.get('/listview', function(req, res) {
+  res.render('listView', { title: 'YEVENGERS-list' });
+});
+
+
 module.exports = router;
