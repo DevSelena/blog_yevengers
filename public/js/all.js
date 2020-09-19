3d7b41f9d2f6e22ee7c5975113d1ba36c8c9ee3e
Jaden <68906446+DevJaden@users.noreply.github.com>
2020-09-19 13:55:24 +0900

    20200919_02

--- public/js/all.js
+++ public/js/all.js
@@ -1,5 +1,4 @@
 window.addEventListener( 'scroll', () =>{
     let scrollLocation = document.documentElement.scrollTop;
-    console.log(scrollLocation);
     scrollLocation > 0 ? document.getElementById('header').classList.add('on') :  document.getElementById('header').classList.remove('on') 
 });
--- public/js/listView.js
+++ public/js/listView.js
@@ -1,29 +1,27 @@
-window.addEventListener('DOMContentLoaded', function(){
+$(function(){
     $.ajax({
         url: '/api/board/find',
         type: 'get',
-        success: function(data){           
+        success: function(data){    
             data.data.data.forEach(function(list) {
-                // let img = "";
-                // if(list.content.split('<img src=').length >=2){
-                //     img = "<img src="+list.content.split('<img src=')[1]
-                // }else{
-                //     img = `<img src="./image/mainVisual.jpg" alt="Thumbnail" />`;
-                // }
+                let imgdata = "";
+                if(list.content.split('<img src=').length >=2){
+                    let img = list.content.split('<img src=')[1]
+                    imgdata = img.split('"')[1];
+                }else{
+                    imgdata = "./image/mainVisual.jpg";
+                }
 
-                let img = (list.content.split('<img src=').length >=2) ? "<img src="+list.content.split('<img src=')[1] : `<img src="./image/mainVisual.jpg" alt="Thumbnail" />`;
-
-                
                 let img_remove = list.content.replace(/<(\/img|img)([^>]*)>/gi,""); 
-                const li =  '<li class="list">\n'+
+                const li =  '<li class="list" data-listId ="'+list._id+'" onclick="link(this)" >\n'+
                                 '<div class="imgBox">\n'+
-                                    img +'\n'+
+                                '<img src="'+imgdata+'" alt="thumbnail" />\n'+
                                 '</div>\n'+
                                 '<div class="txtBox">\n'+
                                     '<div class="top">\n'+
                                         '<div class="title">'+list.title+'</div>\n'+
                                         '<div class="text">'+img_remove+'</div>\n'+
-                                        '<div class="writeDay">2020.09.18</div>\n'+
+                                        '<div class="writeDay">'+list.board_date.split(' ')[0]+'</div>\n'+
                                     '</div>\n'+
                                     '<div class="bottom">\n'+
                                         '<span class="person">by. '+list.name+'</span>\n'+
@@ -32,11 +30,17 @@ window.addEventListener('DOMContentLoaded', function(){
                                 '</div>\n'+
                             '</li>\n';
                 $('#listBox').append(li)
+                $('#listBox').find('img')
             });
-
         },
         error: function(err) {
             console.log(err)
         }
-    })
-})
+    });
+});
+
+//list link이동
+function link(item){
+    let listId = item.dataset.listid;
+    location.href = location.origin + "/view?"+listId;
+}
