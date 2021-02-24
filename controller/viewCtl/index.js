989b94ba46d319939e3066c237ff5f01f42e3924
seooooha <mooo4030@naver.com>
2021-02-24 21:04:18 +0900

    리뉴얼 시작

--- controller/viewCtl/index.js
+++ controller/viewCtl/index.js
@@ -6,7 +6,7 @@ let axios = require('axios');
 router.get('/', function(req, res) {
   res.render('index', { title: 'YEVENGERS' });
 });
-
+ 
 router.get('/listview', function(req, res) {
   const searchData = req.query.search
   searchData ?
